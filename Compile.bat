f0e0d59af4501b624eef09d9e31bdace63d2de2e
crypto117 <46241441+crypto117@users.noreply.github.com>
2019-02-25 14:04:41 -0800

    Update Compile.bat

--- Compile.bat
+++ Compile.bat
@@ -1,2 +1,2 @@
-﻿dotnet publish {system path to TrtlBotSharp.csproj} -c Release -r {runtime environment, example: win10-x64}
+﻿dotnet publish {system path to RhoxBotSharp.csproj} -c Release -r {runtime environment, example: win10-x64}
 pause
